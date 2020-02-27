2d3f259a5c84ada8543a4dce79eaff3b07b6d979
israeldavid76 <israeldavid@gmail.com>
2020-02-27 15:25:13 -0500

    Envio de Notificaciones

---
+++ src/app/notificaciones/creargrupos/creargrupos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creargrupos',
+  templateUrl: './creargrupos.component.html',
+  styleUrls: ['./creargrupos.component.scss']
+})
+export class CreargruposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/notificaciones/notificaciones.component.ts
+++ src/app/notificaciones/notificaciones.component.ts
@@ -3,6 +3,7 @@ import { TabsService } from '../servicios/tabs.service';
 import { responseTabs } from '../interfaces/interface.tabs';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl,FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-notificaciones',
@@ -11,7 +12,19 @@ import { Router } from '@angular/router';
 })
 export class NotificacionesComponent implements OnInit {
   responseTabs: responseTabs;
-  constructor(private ts:TabsService,private sanitized: DomSanitizer,private route:Router) { }
+  public formGroup: FormGroup;
+  constructor(private ts:TabsService,
+              private sanitized: DomSanitizer,
+              private route:Router,
+              private formBuilder: FormBuilder) 
+  { 
+    this.formGroup = formBuilder.group({
+    empresa: ['1'],
+    aplicacion: ['1'],
+    titulo: ['',Validators.required],
+    mensaje:['',Validators.required]
+    });
+  }
 
   ngOnInit() {
     this.consultarTabs();
@@ -28,6 +41,15 @@ export class NotificacionesComponent implements OnInit {
 
   crearGrupo(){
     this.route.navigateByUrl("creargrupo")
+  }
+
+  envioFCM(){
+    if (this.formGroup.valid) {
+      console.log(this.formGroup.value)
+    }
+    else{
+      alert("Llena los campos necesarios")
+    }
   }
 
 }
